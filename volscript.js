55e4691e4a73ae7f0ce980dc94f3ba8546399645
Rameswar-git <rameswarprasaddas@gmail.com>
2020-10-23 11:08:11 +0530

    Back Button Event added and commented code removed

--- volscript.js
+++ volscript.js
@@ -20,52 +20,25 @@ function runTimeoutVolumeReducer() {
     }
   }, 50);
 }
+window.addEventListener("popstate", function () {
+  console.log('Back button Pressed........................');
+  reduceVolume();
+});
 window.addEventListener("load", function () {
   console.log("window loaded..................");
-  setTimeout(function(){
-    if (document.querySelector("video") != null) {
-        document.querySelector("video").volume = 0.1;
-        console.log("volume reduced successfully");
-      } else {
-        console.log("video still not available during window load---------------");
-        runTimeoutVolumeReducer();
-      }
-  },1000)
+  reduceVolume();
 });
 
-// document.addEventListener("readystatechange", function () {
-//     console.log("documment ready..................");
-//     if (document.querySelector("video") != null) {
-//         document.querySelector("video").volume = 0.7;
-//         console.log("volume reduced successfully");
-//       } else {
-//         console.log("video not available during readystatechange---------------");
-//       }
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     console.log("documment loaded..................");
-//     if (document.querySelector("video") != null) {
-//         document.querySelector("video").volume = 0.4;
-//         console.log("volume reduced successfully");
-//       } else {
-//         console.log("video not available during DOMContentLoaded---------------");
-//       }
-// });
-
-// window.onload = function () {
-//   // chrome.windows.getCurrent(function (currentWindow) {
-//   //   chrome.tabs.query({active: true, windowId: currentWindow.id},
-//   //                     function(activeTabs,videosData) {
-//   //                         console.log(JSON.stringify("Hello"));
-//   //         console.log(JSON.stringify(videosData));
-//   //         console.log(JSON.stringify(activeTabs));
-//   //   });
-//   // });
-//   if (document.querySelector("video") != null) {
-//     document.querySelector("video").volume = 0.1;
-//     console.log("volume reduced successfully");
-//   } else {
-//     console.log("video still not vav---------------");
-//   }
-// };
+function reduceVolume(){
+  setTimeout(function () {
+    if (document.querySelector("video") != null) {
+      document.querySelector("video").volume = 0.1;
+      console.log("volume reduced successfully");
+    } else {
+      console.log(
+        "video still not available during window load---------------"
+      );
+      runTimeoutVolumeReducer();
+    }
+  }, 1000);
+}
